ec9aad546579f199fad8e044c62c35cd7f6c0aa7
Connor Smith <titdirt994@gmail.com>
2017-05-23 17:21:00 -0500

    Add files via upload

--- PassGen.py
+++ PassGen.py
@@ -1,1 +1,16 @@
 import random
+
+noun = ('cat','dog','elephant','monkey','zebra')
+adjectives = ('sticky','dumb','orange','red','yellow','small','large')
+
+def genPass():
+    # n is lenth of desired password
+    ranNoun = noun[random.randint(0,len(noun)-1)]
+    ranAdj = adjectives[random.randint(0,len(adjectives)-1)]
+    password = ranAdj + ranNoun.capitalize()
+    return password
+
+# def add(l):
+    
+for i in range(5):
+    print(genPass())
