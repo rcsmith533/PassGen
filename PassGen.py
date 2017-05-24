68ed65d9d04128dd35f0e0536f5dca390ac95479
Connor Smith <titdirt994@gmail.com>
2017-05-23 20:47:46 -0500

    Added function to add nouns and adjectives to the list

--- PassGen.py
+++ PassGen.py
@@ -1,16 +1,31 @@
 import random
 
-noun = ('cat','dog','elephant','monkey','zebra')
-adjectives = ('sticky','dumb','orange','red','yellow','small','large')
+noun = ['cat','dog','elephant','monkey','zebra']
+adjectives = ['sticky','dumb','orange','red','yellow','small','large']
 
 def genPass():
     # n is lenth of desired password
     ranNoun = noun[random.randint(0,len(noun)-1)]
+    num = random.randint(0,999)
     ranAdj = adjectives[random.randint(0,len(adjectives)-1)]
-    password = ranAdj + ranNoun.capitalize()
+    password = ranAdj + ranNoun.capitalize() + str(num)
     return password
 
-# def add(l):
+def menu():
+    print('1) Generate a password\n2) Add a noun\n3) Add an adjective')
     
-for i in range(5):
-    print(genPass())
+def addNoun(l):
+    if l in noun:
+        print(l, 'is already in the list of nouns')
+    else:
+        noun.append(l)
+
+def addAdj(l):
+    if l in adjectives:
+        print(l, 'is already in the list of adjectives')
+    else:
+        adjectives.append(l)
+
+menu()
+addNoun('cat')
+addAdj('dumb')
